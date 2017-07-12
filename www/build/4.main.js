e3c80abbff832ab3b806c1595aff210ec479af81
Kavita Vaish <kavitavaish82@gmail.com>
2017-07-12 15:34:42 +0530

    menu & content margin

--- www/build/4.main.js
+++ www/build/4.main.js
@@ -78,7 +78,7 @@ var HeaderPage = (function () {
 HeaderPage = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* IonicPage */])(),
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* Component */])({
-        selector: 'page-header',template:/*ion-inline-start:"C:\Users\svohra\Desktop\fsm\clientApp\src\pages\header\header.html"*/'<!--\n\n  Generated template for the HeaderPage page.\n\n\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n\n  Ionic pages and navigation.\n\n-->\n\n<ion-header>\n\n\n\n  <ion-navbar>\n\n    <ion-title>header</ion-title>\n\n  </ion-navbar>\n\n\n\n</ion-header>\n\n\n\n\n\n<ion-content padding>\n\n\n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\svohra\Desktop\fsm\clientApp\src\pages\header\header.html"*/,
+        selector: 'page-header',template:/*ion-inline-start:"D:\clientApp\src\pages\header\header.html"*/'<!--\n\n  Generated template for the HeaderPage page.\n\n\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n\n  Ionic pages and navigation.\n\n-->\n\n<ion-header>\n\n\n\n  <ion-navbar>\n\n    <ion-title>header</ion-title>\n\n  </ion-navbar>\n\n\n\n</ion-header>\n\n\n\n\n\n<ion-content padding>\n\n\n\n</ion-content>\n\n'/*ion-inline-end:"D:\clientApp\src\pages\header\header.html"*/,
     }),
     __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavParams */]])
 ], HeaderPage);
